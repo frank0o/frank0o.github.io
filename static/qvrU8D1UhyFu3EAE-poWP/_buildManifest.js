f160e42cd52d654c05d680ed39506a4f52748019
frank0o <frank0o@frank0os-Air.lan>
2022-10-29 15:46:33 +1100

    Updates

--- static/qvrU8D1UhyFu3EAE-poWP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/970e22442d37497d.css","static/chunks/pages/index-4ed7a0e91c99dbe8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
