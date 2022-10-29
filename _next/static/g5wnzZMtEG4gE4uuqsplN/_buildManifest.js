548a1802b60679db0c30f6e9dec61e9ac86d0be4
frank0o <frank0o@frank0os-Air.lan>
2022-10-29 17:28:05 +1100

    new branch

---
+++ _next/static/g5wnzZMtEG4gE4uuqsplN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/336423acba53d382.css","static/chunks/pages/index-9a105bdc525f8b3c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
